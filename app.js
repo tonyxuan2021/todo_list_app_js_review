e981f6aa8a38e441f717fa8079c397fa047f55df
tonyxuan2021 <tonyyexuan@gmail.com>
2022-09-08 15:16:28 -0700

    complete project

--- app.js
+++ app.js
@@ -49,4 +49,10 @@ function deleteCheck(e) {
     const todo = item.parentElement;
     todo.remove();
   }
+
+  // check mark
+  if (item.classList[0] === "complete-btn") {
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
+  }
 }
